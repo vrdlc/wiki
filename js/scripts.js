b397bcaee08805edf9d5136485947de6df4261e2
Jay Whang and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-01-14 11:55:30 -0800

    add three more pages and js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#here").click(function() {
+    $(".codebox1").addClass("codebox1-color");
+    //$(".ok").toggle();
+  });
+  $("#float").click(function(){
+    $("#logo").css("float", "left"); 
+  });
+
+
+});
